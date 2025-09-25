565b0bd0b6a52edcadaec1e5ec1e28de06f29900
Niko Pippuri <niko.pippuri@gmail.com>
2025-09-25 20:05:40 +0300

    Placeholders for userpage

---
+++ frontend/src/features/profile/components/AcceptGroupInvites.jsx
@@ -0,0 +1,8 @@
+export function AcceptGroupInvites() {
+    return (
+        <div>
+            <h1>Accept Group Invites</h1>
+            <button>Accept Group Invites</button>
+        </div>
+    );
+}
---
+++ frontend/src/features/profile/components/RemoveFromGroup.jsx
@@ -0,0 +1,8 @@
+export function RemoveFromGroup() {
+    return (
+        <div>
+            <h1>Remove From Group</h1>
+            <button>Remove From Group</button>
+        </div>
+    );
+}

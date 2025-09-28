2db6aa6b7dd51ee09b06e2fa4ff246547795c5bd
Jamo <jamo.jaakola@gmail.com>
2025-09-28 11:31:12 +0300

    Kayttajan poisto toimii + kirjautumis vaatimukset muutettu englanniksi

--- backend/modules/auth/authRoutes.js
+++ backend/modules/auth/authRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { signup, signin } from './authController.js';
+import { signup, signin, deleteUser } from './authController.js';
 
 const router = Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin);
+router.delete('/delete/:id', deleteUser);
 
 export default router;
